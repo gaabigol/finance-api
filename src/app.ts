67036cebc7fb25a3d127a9c3b2a49292b451e1ad
gabigol_96@icloud.com <gabigol_96@icloud.com>
2025-02-18 18:11:45 +0000

    chore: ajustar configurações husky

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,10 @@ app.get('/', (req: Request, res: Response) => {
     res.json({ message: 'API funcionando!' })
 })
 
+app.get('/hello', (req: Request, res: Response) => {
+    res.json({ message: 'Olá, mundo!' })
+})
+
 app.listen(port, () => {
     console.log(`⚡️[servidor]: API está rodando em http://localhost:${port}`)
 })
